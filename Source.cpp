8d6822567b247ae4c6eeb6f3b89dd4729025f913
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-11-15 14:51:50 +0200

    done

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int size = 5;
+    int arr1[size] = { 1, 2, 3, 4, 5 };
+    int arr2[size];
+
+    int* ptr1 = arr1;
+    int* ptr2 = arr2;
+
+    for (int i = 0; i < size; ++i) {
+        *ptr2 = *ptr1;
+        ptr1++;
+        ptr2++;
+    }
+
+    cout << "Copied Array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
